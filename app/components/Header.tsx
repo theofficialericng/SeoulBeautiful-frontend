d42b8ab8eca77d09757b374b5caacbb4bf04a113
garry014 <garychia14@gmail.com>
2025-02-20 15:07:01 +0800

    Reset logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,12 +6,6 @@ import { Menu, X, User, Inbox } from "lucide-react"
 import { useAuth } from "../contexts/AuthContext"
 import Image from "next/image"
 
-// inline CSS styling
-const imageStyle = {
-  borderRadius: '50%',
-  border: '5px solid #fff',
-}
-
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const { user, logout } = useAuth()
@@ -20,9 +14,14 @@ const Header = () => {
     <header className="bg-white shadow-md">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center py-4">
-          <Image src="/images/seoul-beautiful-icon.webp" width={120} height={120} alt="Website Icon" style={imageStyle}/>
-          <Link href="/" className="text-4xl font-bold text-gray-800 mr-auto tracking-wide" >
-            Seoul Beautiful
+          <Link href="/" className="text-2xl font-bold text-gray-800">
+            
+            <Image
+              src="/images/logos/navbar.png"
+              alt="Seoul Beautiful"
+              width="150"
+              height="50"
+            />
           </Link>
           <nav className="hidden md:flex space-x-8">
             <Link href="/procedures" className="text-gray-600 hover:text-gray-800">
