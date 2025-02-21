18a635579020c14bf4b3f13e3003f8073c0429e3
garry014 <garychia14@gmail.com>
2025-02-21 22:35:58 +0800

    maze integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import { AuthProvider } from "./contexts/AuthContext"
 import type React from "react"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,12 +22,35 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      
         <AuthProvider>
           <Header />
           <main>{children}</main>
           <Footer />
         </AuthProvider>
+
+        <Script id="maze-script" strategy="afterInteractive">
+          {`
+            (function (m, a, z, e) {
+              var s, t;
+              try {
+                t = m.sessionStorage.getItem('maze-us');
+              } catch (err) {}
+
+              if (!t) {
+                t = new Date().getTime();
+                try {
+                  m.sessionStorage.setItem('maze-us', t);
+                } catch (err) {}
+              }
+
+              s = a.createElement('script');
+              s.src = z + '?apiKey=' + e;
+              s.async = true;
+              a.getElementsByTagName('head')[0].appendChild(s);
+              m.mazeUniversalSnippetApiKey = e;
+            })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '80ae5902-3e38-4cb2-932a-d2d1d5799323');
+          `}
+        </Script>
       </body>
     </html>
   )
